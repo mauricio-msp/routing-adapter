2d256282a20c1e7d0bbef46cb2a78442eaef5cf3
Maurício Porfírio <mauriciosporfirio@gmail.com>
2017-06-06 00:24:37 -0300

    Rotas GET|POST|PUT|DELETE acrescentadas

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -8,33 +8,87 @@ class Route extends \Klein\Klein {
     
     /**
      * -------------------------------------------------------------------------
-     * Rota Adaptada
+     * Rotas Adaptadas
      * -------------------------------------------------------------------------
      * 
-     * Essa função cria uma rota que pode ser dos tipos: GET|POST|PUT|DELETE.
-     * Verifica também se o 3º parâmetro é do tipo string class->method(), uma
+     * Rotas que podem ser dos tipos: GET|POST|PUT|DELETE.
+     * Verifica também se o 2º parâmetro é do tipo string class->method(), uma
      * classe instanciada com chamada de método. Caso não seja ele interpreta o 
-     * padrão que é uma function. 
+     * padrão que é uma function(){}. 
      * 
-     * @param type $method
      * @param type $path
      * @param type $callback
      */
     
-    public function map($method, $path = '*', $callback = NULL){
+    
+    public function get($path = '*', $callback = NULL){
         if(is_string($callback)):
             
             $params     = explode('@', $callback);
             $controller = /* App\\Controllers\\  . */ $params[0];
             $action     = $params[1];
             
-            $this->respond($method, $path, function($request, $response, $app) use ($controller, $action){
+            $this->respond('GET', $path, function($request, $response, $app) use ($controller, $action){
                 $controller = new $controller();
                 $controller->loader($request, $response, $app);
                 return $controller->$action();
             });
         else:
-            $this->respond($method, $path, $callback);
+            $this->respond('GET', $path, $callback);
+        endif;
+    }
+    
+    
+    public function post($path = '*', $callback = NULL){
+        if(is_string($callback)):
+            
+            $params     = explode('@', $callback);
+            $controller = /* App\\Controllers\\  . */ $params[0];
+            $action     = $params[1];
+            
+            $this->respond('POST', $path, function($request, $response, $app) use ($controller, $action){
+                $controller = new $controller();
+                $controller->loader($request, $response, $app);
+                return $controller->$action();
+            });
+        else:
+            $this->respond('POST', $path, $callback);
+        endif;
+    }
+    
+    
+    public function put($path = '*', $callback = NULL){
+        if(is_string($callback)):
+            
+            $params     = explode('@', $callback);
+            $controller = /* App\\Controllers\\  . */ $params[0];
+            $action     = $params[1];
+            
+            $this->respond('PUT', $path, function($request, $response, $app) use ($controller, $action){
+                $controller = new $controller();
+                $controller->loader($request, $response, $app);
+                return $controller->$action();
+            });
+        else:
+            $this->respond('PUT', $path, $callback);
+        endif;
+    }
+    
+    
+    public function delete($path = '*', $callback = NULL){
+        if(is_string($callback)):
+            
+            $params     = explode('@', $callback);
+            $controller = /* App\\Controllers\\  . */ $params[0];
+            $action     = $params[1];
+            
+            $this->respond('DELETE', $path, function($request, $response, $app) use ($controller, $action){
+                $controller = new $controller();
+                $controller->loader($request, $response, $app);
+                return $controller->$action();
+            });
+        else:
+            $this->respond('DELETE', $path, $callback);
         endif;
     }
     
